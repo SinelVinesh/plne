49f58d44016ba5c99fc6fe0503af8305fe06b871
Sinel Vinesh <vinesh.sinel@gmail.com>
2024-06-19 18:55:04 +0300

    Add maximum flow problem solver using Ford Flukerson algo at /ford-flukerson

--- src/ford-fulkerson/FordFulkerson.tsx
+++ src/ford-fulkerson/FordFulkerson.tsx
@@ -2,7 +2,7 @@ import "./FordFulkerson.css"
 import {useEffect, useState} from "react";
 import {FlowNode, MaximumFlowData, getMaximumFlowData} from "./fordFulkerson.ts";
 
-const defaultNodeNumber = 3
+const defaultNodeNumber = 6
 
 function FordFulkerson() {
   const [graph, setGraph] = useState<FlowNode[][]>([])
@@ -16,9 +16,9 @@ function FordFulkerson() {
       newGraph.push([] as FlowNode[])
       for (let j = 0; j < n; j++) {
         if (i === j) {
-          newGraph[i].push({ flow: 0, capacity: 0 })
+          newGraph[i].push({ flow: 0, capacity: 0, visited: false})
         }
-        newGraph[i].push({ flow: NaN, capacity: NaN })
+        newGraph[i].push({ flow: NaN, capacity: NaN, visited: false })
       }
     }
     setGraph(newGraph)
@@ -27,11 +27,12 @@ function FordFulkerson() {
   const updateGraph = (i: number, j: number, flow: number|undefined, capacity: number|undefined) => {
     const newGraph = graph
     if (newGraph[i][j] === undefined) {
-      newGraph[i][j] = { flow: flow ?? 0, capacity: capacity ?? 0 }
+      newGraph[i][j] = { flow: flow ?? 0, capacity: capacity ?? 0, visited: false }
     } else {
       const node = newGraph[i][j]
-      newGraph[i][j] = { flow: flow ?? node.flow, capacity: capacity ?? node.capacity }
+      newGraph[i][j] = { flow: flow ?? ((isNaN(node.flow) && !isNaN(node.capacity)) ? 0 : node.flow), capacity: capacity ?? node.capacity, visited: node.visited }
     }
+    document.getElementById(`arc-${i}-${j}-flow`)?.setAttribute("value", isNaN(newGraph[i][j].flow) ? "NaN" : newGraph[i][j].flow.toString())
     setGraph(newGraph)
   }
 
@@ -45,9 +46,9 @@ function FordFulkerson() {
       for (let j = 0; j < n; j++) {
         if (newGraph[i][j] === undefined) {
           if (i === j) {
-            newGraph[i].push({ flow: 0, capacity: 0 })
+            newGraph[i].push({ flow: 0, capacity: 0, visited: false })
           } else {
-            newGraph[i].push({ flow: NaN, capacity: NaN })
+            newGraph[i].push({ flow: NaN, capacity: NaN, visited: false })
           }
         }
       }
@@ -61,7 +62,7 @@ function FordFulkerson() {
     expandGraph(n)
     const initialCharCode = "A".charCodeAt(0)
     if (!isNaN(n) && n > 0) {
-      const theadRow = document.querySelector("thead>tr")
+      const theadRow = document.querySelector("#input>thead>tr")
       if (theadRow) {
         theadRow.innerHTML = "<th>-></th>"
         for (let i = 0; i < n; i++) {
@@ -71,7 +72,7 @@ function FordFulkerson() {
         }
       }
     }
-    const tbody = document.querySelector("tbody")
+    const tbody = document.querySelector("#input>tbody")
     if (tbody) {
       tbody.innerHTML = ""
       for (let i = 0; i < n; i++) {
@@ -96,13 +97,65 @@ function FordFulkerson() {
             const capacityInput = document.createElement("input")
             capacityInput.type = "number"
             capacityInput.id = `arc-${i}-${j-1}-capacity`
-            capacityInput.onchange = (e) => {updateGraph(i,j-1,undefined,parseInt((e.target as HTMLInputElement).value))}
+            capacityInput.onchange = (e) => {
+              updateGraph(i,j-1,undefined,parseInt((e.target as HTMLInputElement).value))
+            }
             if (graph[i][j-1] !== undefined) {
               capacityInput.setAttribute("value", graph[i][j-1].capacity.toString())
             }
             if (i === j-1) {
               capacityInput.setAttribute("disabled", "disabled")
             }
+            const separator = document.createElement("span")
+            separator.textContent = "/"
+            td.appendChild(flowInput)
+            td.appendChild(separator)
+            td.appendChild(capacityInput)
+            tr.appendChild(td)
+          }
+        }
+        tbody.appendChild(tr)
+      }
+    }
+  }
+
+  const updateOutput = (n: number) => {
+    const initialCharCode = "A".charCodeAt(0)
+    if (!isNaN(n) && n > 0) {
+      const theadRow = document.querySelector("#output>thead>tr")
+      if (theadRow) {
+        theadRow.innerHTML = "<th>-></th>"
+        for (let i = 0; i < n; i++) {
+          const th = document.createElement("th")
+          th.textContent = String.fromCharCode(initialCharCode + i)
+          theadRow.appendChild(th)
+        }
+      }
+    }
+    const tbody = document.querySelector("#output>tbody")
+    if (tbody) {
+      tbody.innerHTML = ""
+      for (let i = 0; i < n; i++) {
+        const tr = document.createElement("tr")
+        for (let j = 0; j < n+1; j++) {
+          if (j === 0) {
+            const th = document.createElement("th")
+            th.textContent = String.fromCharCode(initialCharCode + i)
+            tr.appendChild(th)
+          } else {
+            const td = document.createElement("td")
+            const flowInput = document.createElement("input")
+            flowInput.type = "number"
+            if (maximumFlowData?.graph[i][j-1] !== undefined) {
+              flowInput.setAttribute("value", maximumFlowData?.graph[i][j-1].flow.toString())
+            }
+            flowInput.setAttribute("disabled", "disabled")
+            const capacityInput = document.createElement("input")
+            capacityInput.type = "number"
+            if (maximumFlowData?.graph[i][j-1] !== undefined) {
+              capacityInput.setAttribute("value", maximumFlowData?.graph[i][j-1].capacity.toString())
+            }
+            capacityInput.setAttribute("disabled", "disabled")
             const separator = document.createElement("span")
             separator.textContent = "/"
             td.appendChild(flowInput)
@@ -124,6 +177,9 @@ function FordFulkerson() {
     initGraph(defaultNodeNumber)
     updateTable(defaultNodeNumber)
   }, []);
+  useEffect(() => {
+    updateOutput(maximumFlowData?.graph.length ?? 0)
+  }, [maximumFlowData]);
 
   return (
     <>
@@ -133,7 +189,7 @@ function FordFulkerson() {
           <label htmlFor="">Nombre de noeuds</label>
           <input className="border-solid border-2 px-1 w-12" type="number" id="nodenumber" defaultValue={defaultNodeNumber} onChange={(e) => updateTable(parseInt(e.target.value))}/>
         </div>
-        <table className="mb-8">
+        <table id="input" className="mb-8">
           <thead>
           <tr>
             <th>-{">"}</th>
@@ -161,8 +217,25 @@ function FordFulkerson() {
       </div>
       {maximumFlowData != undefined &&
         <div>
-          <h2>Maximum flow</h2>
-          <p>{maximumFlowData.maxFlow}</p>
+          <h2 className="mt-8 mb-4 font-bold text-2xl">Results</h2>
+          <div className="flex flex-col items-start">
+            <div>
+              <p className="text-lg"><span className="font-bold ">Tableau final</span></p>
+              <table id="output" className="mb-8">
+                <thead>
+                <tr>
+                  <th>-{">"}</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+              </table>
+            </div>
+            <div>
+              <p className="text-lg"><span className="font-bold ">Maximum flow : </span>{maximumFlowData.maxFlow}</p>
+            </div>
+          </div>
         </div>
       }
     </>
--- src/ford-fulkerson/fordFulkerson.ts
+++ src/ford-fulkerson/fordFulkerson.ts
@@ -1,6 +1,7 @@
 export type FlowNode = {
     flow: number,
     capacity: number,
+    visited: boolean,
 }
 
 export type MaximumFlowData = {
@@ -13,23 +14,70 @@ export type MaximumFlowData = {
 type AdvancedPathArc = {
     from: number,
     to: number,
-    flow: number,
+    gap: number,
     mark: "+" | "-"
 }
 
 export function getMaximumFlowData(graph: FlowNode[][], source: number, sink: number): MaximumFlowData {
-    let avancedPath = findAdvancedPath(graph, source, sink)
-    while (avancedPath != undefined) {
-
+    const graphCopy = copyGraph(graph)
+    let advancedPath = findAdvancedPath(graphCopy, source, sink, [])
+    while (advancedPath != undefined) {
+        let minFlow = Infinity
+        for (const arc of advancedPath) {
+            minFlow = Math.min(minFlow, arc.gap)
+        }
+        for (const arc of advancedPath) {
+            if (arc.mark === "+") {
+                graphCopy[arc.from][arc.to].flow += minFlow
+            } else {
+                graphCopy[arc.to][arc.from].flow -= minFlow
+            }
+        }
+        advancedPath = findAdvancedPath(graphCopy, source, sink, [])
     }
+    return {graph: graphCopy, source: source, sink: sink, maxFlow: graphCopy[source].reduce((acc, node) => acc + (isNaN(node.flow) ? 0 : node.flow), 0)}
 }
 
-function findAdvancedPath(graph: FlowNode[][], source: number, sink: number): AdvancedPathArc[] | undefined {
-    const result: AdvancedPathArc[] = []
-    for (const row of graph) {
-        const gap = row[source].capacity - row[source].flow
-        if (!isNaN(row[source].capacity) && gap > 0)  {
-
+function findAdvancedPath(graph: FlowNode[][], current: number, sink: number, path: AdvancedPathArc[]): AdvancedPathArc[] | undefined {
+    if (current === sink) {
+        return path
+    }
+    const currentRow = graph[current]
+    for (let i = 0; i < currentRow.length; i++) {
+        const gap = currentRow[i].capacity - currentRow[i].flow
+        if (i != current && !isNaN(currentRow[i].capacity) && gap > 0 && !currentRow[i].visited) {
+            path.push({ from: current, to: i, gap: gap, mark: "+" })
+            currentRow[i].visited = true
+            const result = findAdvancedPath(graph, i, sink, path)
+            currentRow[i].visited = false
+            if (result != undefined) {
+                return result
+            }
+            path.pop()
         }
     }
+    for (let i = 0; i < graph.length; i++) {
+        if (i != current && !isNaN(graph[i][current].capacity) && graph[i][current].flow > 0 && !graph[i][current].visited) {
+            path.push({from: i, to: current, gap: graph[i][current].flow, mark: "-"})
+            graph[i][current].visited = true
+            const result = findAdvancedPath(graph, i, sink, path)
+            currentRow[i].visited = false
+            if (result != undefined) {
+                return result
+            }
+            path.pop()
+        }
+    }
+    return undefined
+}
+
+function copyGraph(graph: FlowNode[][]): FlowNode[][] {
+    const newGraph = []
+    for (let i = 0; i < graph.length; i++) {
+        newGraph.push([] as FlowNode[])
+        for (let j = 0; j < graph[i].length; j++) {
+            newGraph[i].push({flow: graph[i][j].flow, capacity: graph[i][j].capacity, visited: false})
+        }
+    }
+    return newGraph
 }
